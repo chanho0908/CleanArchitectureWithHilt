13f6faccc91a6c5c1253d4a43f58673ccdc0d44b
chanho <uni070@naver.com>
2024-05-08 09:12:15 +0900

    [UI] 메인 화면 UI 변경

---
+++ presentation/src/main/java/kr/suwon/chanho/presentation/theme/MainActivity.kt
@@ -0,0 +1,26 @@
+package kr.suwon.chanho.presentation.theme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
