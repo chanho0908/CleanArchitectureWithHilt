be710dcd9b3d7f07b5da394a486a3f0db8f01738
chanho <uni070@naver.com>
2024-05-09 16:51:11 +0900

    [Feat] User Token check UseCase ViewModel 분리

--- presentation/src/main/java/kr/suwon/chanho/presentation/splash/SplashActivity.kt
+++ presentation/src/main/java/kr/suwon/chanho/presentation/splash/SplashActivity.kt
@@ -2,41 +2,47 @@ package kr.suwon.chanho.presentation.splash
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import kr.suwon.chanho.domain.usecase.login.GetTokenUseCase
 import kr.suwon.chanho.presentation.login.LoginActivity
 import kr.suwon.chanho.presentation.main.MainActivity
+import kr.suwon.chanho.presentation.splash.vm.SplashViewModel
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity: AppCompatActivity(){
-    @Inject
-    lateinit var getTokenUseCase: GetTokenUseCase
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         lifecycleScope.launch {
-            val isLoggedIn = !getTokenUseCase().isNullOrEmpty()
-
-            if (isLoggedIn){
-                startActivity(
-                    Intent(
-                        this@SplashActivity, MainActivity::class.java
-                    ).apply {
-                        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.token.collect{
+                    if (!it.isNullOrEmpty()){
+                        startActivity(
+                            Intent(
+                                this@SplashActivity, MainActivity::class.java
+                            ).apply {
+                                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                            }
+                        )
+                    }else{
+                        startActivity(
+                            Intent(
+                                this@SplashActivity, LoginActivity::class.java
+                            ).apply {
+                                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                            }
+                        )
                     }
-                )
-            }else{
-                startActivity(
-                    Intent(
-                        this@SplashActivity, LoginActivity::class.java
-                    ).apply {
-                        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                    }
-                )
+                }
             }
         }
     }
