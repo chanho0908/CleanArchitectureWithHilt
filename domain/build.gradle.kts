592500f51096e94984710aff20144bbc7dbea259
chanho <uni070@naver.com>
2024-05-12 15:07:46 +0900

    [Build] Domain - serialization dependencies 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    alias(libs.plugins.org.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies{
+    implementation(libs.kotlinx.serialization.json)
 }
