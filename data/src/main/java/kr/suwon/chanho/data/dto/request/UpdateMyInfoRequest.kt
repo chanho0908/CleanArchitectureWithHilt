ae545ac02c3b9ad4e4462e3824810e2e9d215e8f
chanho <uni070@naver.com>
2024-05-12 15:35:29 +0900

    [Feat] 사용자 정보 변경을 위한 DTO

---
+++ data/src/main/java/kr/suwon/chanho/data/dto/request/UpdateMyInfoRequest.kt
@@ -0,0 +1,18 @@
+package kr.suwon.chanho.data.dto.request
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+
+@Serializable
+data class UpdateMyInfoRequest (
+    val userName:String,
+    val extraUserInfo:String,
+    val profileFilePath:String,
+)
+
+fun UpdateMyInfoRequest.toRequestBody(): RequestBody{
+    return Json.encodeToString(this).toRequestBody()
+}
